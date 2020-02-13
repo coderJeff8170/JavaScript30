6dde3634aa4d1e6e01ab5471ef7524c76b08ed35
Jeff Andrews <jeff@paintedbyjeff.com>
2020-02-13 16:21:06 -0500

    finished video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,9 +3,9 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const controls = player.querySelector('.player__controls');
-const progress = controls.querySelector('.progress');
-const progressFilled = progress.querySelector('.progress__filled');
-const toggle = controls.querySelector('.toggle');
+const progress = player.querySelector('.progress');
+const progressFilled = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
 //const skipFwd = controls.querySelector('.player__button[data-skip="25"]');
 //const skipBwd = controls.querySelector('.player__button[data-skip="-10"]');
 const skipButtons = player.querySelectorAll('[data-skip]');
@@ -41,6 +41,13 @@ function handleProgress() {
     progressFilled.style.flexBasis = `${percent}%`;
 }
 
+function scrub (e) {
+    console.log(`${e.offsetX}, ${e.offsetY}`);
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    //^^ this interesting tidbit - the division produces a percentage of the played video, which you then multiply by the duration of the video - so if it's 50%, it's 50% OF the video.duration(length of the clip); Clever!
+    video.currentTime = scrubTime;//instantly sets the progressBar to where you clicked!
+}
+
 
 //event listeners
 
@@ -55,3 +62,11 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));//how fucking cool! You can forEach the node list, and add even listeners with a fat arrow function!
 ranges.forEach(range => range.addEventListener('change', handleRanges));
 ranges.forEach(range => range.addEventListener('mousemove', handleRanges));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));//works great! but we want it to happen only when dragging so.. flag variable^^ mousedown starts out false!
+//notice that we're using the 'truthiness' of the logical AND to hijack functionality - scrub will only work if both expressions are true. If mousedown is false, scrub never gets called... Remember to put that event in there, as we need the offset values from it...If you need info from a click, you gotta get the info from the event.
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+//oh my god will I ever be able to think like this???
